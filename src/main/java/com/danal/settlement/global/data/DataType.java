fba5aac0a1159ceb677ae8857ecd94b56e5607d0
jeongseung-yeong <tmddud333@naver.com>
2024-02-16 01:06:22 +0900

    init

---
+++ src/main/java/com/danal/settlement/global/data/DataType.java
@@ -0,0 +1,10 @@
+package com.danal.settlement.global.data;
+
+public enum DataType {
+
+        csv, txt, json;
+
+//        public static DataType fromString(String type) {
+//                return valueOf(type.toUpperCase());
+//        }
+}
